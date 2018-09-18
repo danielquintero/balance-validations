b0ff154158c2ac0f0810cea845849dc51a913b21
Daniel Piedra Quintero <danielpiedraquintero@Daniels-MacBook-Pro.local>
2018-09-18 11:37:28 +0200

    feat(validation): add validation for balance mutations

--- src/app/file/validator/validator.service.ts
+++ src/app/file/validator/validator.service.ts
@@ -8,19 +8,53 @@ import {filter, find, includes, map} from 'lodash-es';
 })
 export class StatementValidatorService {
 	public validate(records: MT940[]): Observable<CustomerStatement[]> {
+		const checkedRefs = this.checkRefs(records);
+		const checkedBalance = this.checkBalance(checkedRefs);
+		return from([checkedBalance]);
+	}
+
+	private checkRefs(records: MT940[]): CustomerStatement[] {
 		const duplicateRefs = filter(records, (value: MT940, index: number, iteratee: MT940[]) => {
 			return find(iteratee, (v: MT940) => v.reference === value.reference, index + 1);
 		});
 		const rest = filter(records, (record) => !includes(duplicateRefs, record));
 		map(duplicateRefs, (value: MT940) => ({...value}));
 
-		const customerStatement = [
+		return [
 			...(map(duplicateRefs, (record: MT940) => ({
 				...record,
 				validation: {isValid: false, errors: ['Duplicated statement reference']}
 			})) as CustomerStatement[]),
 			...(map(rest, (record: MT940) => ({...record, validation: {isValid: true}})) as CustomerStatement[])
 		];
-		return from([customerStatement]);
+	}
+
+	private checkBalance(records: CustomerStatement[]): CustomerStatement[] {
+		return records.map((record) => {
+			let isValid: boolean;
+			const {startBalance, endBalance, mutation, validation} = record;
+			const sign = Math.sign(mutation);
+
+			if (sign === NaN) {
+				throw new Error('Cannot find the symbol for the mutation type');
+			}
+
+			if (sign === 1) {
+				isValid = startBalance + mutation === endBalance;
+			} else if (sign === -1) {
+				isValid = startBalance - Math.abs(mutation) === endBalance;
+			}
+
+			if (isValid) {
+				return record;
+			}
+			// set record validity to false
+			record.validation.isValid = false;
+			const error = 'End Balance is incorrect, wrong mutation applied';
+			validation.errors && validation.errors.length > 0
+				? record.validation.errors.push(error)
+				: (record.validation.errors = [error]);
+			return record;
+		});
 	}
 }
