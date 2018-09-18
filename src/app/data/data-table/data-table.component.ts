8ca5c55e770fa45fd2311d3703ef10e33014c177
Daniel Piedra Quintero <danielpiedraquintero@Daniels-MacBook-Pro.local>
2018-09-18 11:39:00 +0200

    feat(table): add data from store to table component

--- src/app/data/data-table/data-table.component.ts
+++ src/app/data/data-table/data-table.component.ts
@@ -1,25 +1,9 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {MatSort, MatTableDataSource} from '@angular/material';
-
-export interface PeriodicElement {
-	name: string;
-	position: number;
-	weight: number;
-	symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-	{position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-	{position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-	{position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-	{position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-	{position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-	{position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-	{position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-	{position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-	{position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-	{position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'}
-];
+import {Store, select} from '@ngrx/store';
+import * as fromRoot from '../../reducers';
+import {CustomerStatement} from '@rabo/file/statement.model';
+import {Observable} from 'rxjs';
 
 @Component({
 	selector: 'app-data-table',
@@ -27,13 +11,35 @@ const ELEMENT_DATA: PeriodicElement[] = [
 	styleUrls: ['./data-table.component.css']
 })
 export class DataTableComponent implements OnInit {
-	displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-	dataSource = new MatTableDataSource(ELEMENT_DATA);
+	public records$: Observable<CustomerStatement[]>;
+	public displayedColumns: string[];
+	public dataSource: MatTableDataSource<any>;
 
 	@ViewChild(MatSort)
 	sort: MatSort;
 
+	constructor(private store: Store<fromRoot.State>) {}
+
 	ngOnInit() {
-		this.dataSource.sort = this.sort;
+		this.displayedColumns = [
+			'reference',
+			'accountNumber',
+			'startBalance',
+			'mutation',
+			'description',
+			'endBalance',
+			'validation'
+		];
+		this.init();
+	}
+
+	private init() {
+		this.records$ = this.store.pipe(select(fromRoot.getRecords));
+		this.records$.subscribe((records: CustomerStatement[]) => {
+			if (records) {
+				this.dataSource = new MatTableDataSource(records);
+				this.dataSource.sort = this.sort;
+			}
+		});
 	}
 }
